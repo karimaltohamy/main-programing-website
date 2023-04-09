0a0c91f085143355c6e0119db27f53f335c2319d
Karim Altohamy <karimbadr2003@gmail.com>
2023-04-09 03:19:25 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -10,6 +10,10 @@ btnClose.addEventListener("click", () => {
   menu.classList.remove("active");
 });
 
+menu.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+
 // swiper
 window.addEventListener("resize", () => {
   if (innerWidth < 420) {
@@ -70,3 +74,189 @@ function openTab(link, id) {
   tabLink.classList.add("active");
   linkItem.classList.add("active");
 }
+
+// dark and light mode
+const checkbox = document.getElementById("checkbox");
+
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("light");
+  if (document.body.classList.length > 0) {
+    (async () => {
+      await loadPolygonPath(tsParticles);
+
+      await tsParticles.load({
+        particles: {
+          color: {
+            value: "#722ffb",
+            animation: {
+              enable: true,
+              speed: 10,
+            },
+          },
+          move: {
+            attract: {
+              enable: false,
+              distance: 100,
+              rotate: {
+                x: 2000,
+                y: 2000,
+              },
+            },
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "destroy",
+            },
+            path: {
+              clamp: false,
+              enable: true,
+              delay: {
+                value: 0,
+              },
+              generator: "polygonPathGenerator",
+              options: {
+                sides: 6,
+                turnSteps: 30,
+                angle: 30,
+              },
+            },
+            random: false,
+            speed: 3,
+            straight: false,
+            trail: {
+              fillColor: "#fff",
+              length: 20,
+              enable: true,
+            },
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 0,
+          },
+          opacity: {
+            value: 1,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: 2,
+          },
+        },
+        background: {
+          color: "#fff",
+        },
+        fullScreen: {
+          zIndex: -1,
+        },
+        emitters: {
+          direction: "none",
+          rate: {
+            quantity: 1,
+            delay: 0.25,
+          },
+          size: {
+            width: 0,
+            height: 0,
+          },
+          position: {
+            x: 50,
+            y: 50,
+          },
+        },
+      });
+    })();
+  } else {
+    (async () => {
+      await loadPolygonPath(tsParticles);
+
+      await tsParticles.load({
+        particles: {
+          color: {
+            value: "#722ffb",
+            animation: {
+              enable: true,
+              speed: 10,
+            },
+          },
+          move: {
+            attract: {
+              enable: false,
+              distance: 100,
+              rotate: {
+                x: 2000,
+                y: 2000,
+              },
+            },
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "destroy",
+            },
+            path: {
+              clamp: false,
+              enable: true,
+              delay: {
+                value: 0,
+              },
+              generator: "polygonPathGenerator",
+              options: {
+                sides: 6,
+                turnSteps: 30,
+                angle: 30,
+              },
+            },
+            random: false,
+            speed: 3,
+            straight: false,
+            trail: {
+              fillColor: "#000",
+              length: 20,
+              enable: true,
+            },
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 0,
+          },
+          opacity: {
+            value: 1,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: 2,
+          },
+        },
+        background: {
+          color: "#000",
+        },
+        fullScreen: {
+          zIndex: -1,
+        },
+        emitters: {
+          direction: "none",
+          rate: {
+            quantity: 1,
+            delay: 0.25,
+          },
+          size: {
+            width: 0,
+            height: 0,
+          },
+          position: {
+            x: 50,
+            y: 50,
+          },
+        },
+      });
+    })();
+  }
+});
